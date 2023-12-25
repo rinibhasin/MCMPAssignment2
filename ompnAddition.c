686717900b1abbcf547f8c0bb2bd77694c046abc
rinibhasin <rinibhasin17july@gmail.com>
2023-12-25 13:21:42 +0000

    C Inserstion add tour length

--- ompnAddition.c
+++ ompnAddition.c
@@ -1,26 +1,27 @@
+#include<omp.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+#include <float.h>
 #include<string.h>
 #include<stdbool.h>
-#include<math.h>
-#include<omp.h>
-#include <float.h>
-
 
 struct TourData {
     int* tour;
     double tourSize;
 };
 
-
-struct TourData nearestAddition(double **distances, int numOfCoords, int startingNode) {
+struct TourData nearestAddition(double **distanceMatrix, int numOfCoords, int startingNode){
 
     int visitedCount = 0;
+
+    // Memory allocation for tour and visited Array
     int *tour = malloc((numOfCoords + 1) * sizeof(int));
     bool *visited = malloc(numOfCoords * sizeof(bool));
 
-    int p =0;
-    for(p =0; p< numOfCoords; p++)
+    //Initialising tour and visited array to empty
+    int p=0;
+    for(p=0; p<numOfCoords; p++)
     {
      tour[p] = 0;
      visited[p] = false;
@@ -28,9 +29,10 @@ struct TourData nearestAddition(double **distances, int numOfCoords, int startin
 
     tour[numOfCoords] = 0;
 
+
+
+
     int nearest;
-    struct TourData result;
-    result.tour = malloc((numOfCoords + 1) * sizeof(int));
 
     int noOfThreads = omp_get_max_threads();
 
@@ -38,52 +40,64 @@ struct TourData nearestAddition(double **distances, int numOfCoords, int startin
     int *positions = (int *) malloc(noOfThreads * sizeof(int));
     int *nearestNodes = (int *) malloc(noOfThreads * sizeof(int));
 
+    // Starting tour with node received from main method
     tour[0] = startingNode;
+    // Marked the node as visited and increased count of visited nodes
     visited[startingNode] = true;
     visitedCount++;
 
     double minimumDistance = DBL_MAX;
 
-    for (int i = 0; i < numOfCoords; i++) {
-        if (!visited[i]) {
-            if (distances[startingNode][i] < minimumDistance) {
-                minimumDistance = distances[startingNode][i];
+    // Finding the nearest vertex
+    for (int i = 0; i < numOfCoords; i++)
+    {
+        if (!visited[i])
+        {
+            if (distanceMatrix[startingNode][i] < minimumDistance)
+            {
+                minimumDistance = distanceMatrix[startingNode][i];
                 nearest = i;
             }
         }
     }
 
+    // Adding the nearest vertex in the tour
     tour[1] = nearest;
     visited[nearest] = true;
     visitedCount++;
 
+    // Adding the starting node again to complete the partial tour
     tour[2] = startingNode;
 
     while (visitedCount < numOfCoords)
     {
-        double minAdditionCost = DBL_MAX;
-        double minimum_Cost = DBL_MAX;
-        int min_position;
-        int min_Unvisited_node;
-        int positionToAdd, position;
         int y=0;
-        double max = DBL_MAX;
-        int threadID;
-
-        for (y = 0; y < noOfThreads; y++) {
+        for (y = 0; y <noOfThreads; y++)
+        {
             minimumAdditionalCosts[y] = DBL_MAX;
             positions[y] = 0;
             nearestNodes[y] = 0;
         }
-        int i=0, j=0;
-        #pragma omp parallel for collapse(2) private(i, j, threadID) shared(visited, distances, minimumAdditionalCosts, positions, nearestNodes)
-        for ( i = 0; i < visitedCount; i++) {
-            for ( j = 0; j < numOfCoords; j++) {
-                threadID = omp_get_thread_num();
-                if (!visited[j]) {
 
-                    double additionalCost = distances[tour[i]][j];
-                    if (additionalCost < minimumAdditionalCosts[threadID]) {
+        double minimumAdditionalCost = DBL_MAX;
+        int minN;
+        int minUnvisited;
+        int positionToAdd, position;
+
+        int i=0, j=0;
+        int threadID;
+
+        #pragma omp parallel for collapse(2) private(i, j, threadID) shared(visited, distanceMatrix, minimumAdditionalCosts, positions, nearestNodes)
+        for ( i = 0; i < visitedCount; i++)
+        {
+            for ( j = 0; j < numOfCoords; j++)
+            {
+                threadID = omp_get_thread_num();
+                if (!visited[j])
+                {
+                    double additionalCost = distanceMatrix[tour[i]][j];
+                    if (additionalCost < minimumAdditionalCosts[threadID])
+                    {
                         minimumAdditionalCosts[threadID] = additionalCost;
                         positions[threadID] = i;
                         nearestNodes[threadID] = j;
@@ -94,71 +108,66 @@ struct TourData nearestAddition(double **distances, int numOfCoords, int startin
 
 
         int x = 0;
-        for (x = 0; x < noOfThreads; x++) {
+        for (x = 0; x < noOfThreads; x++)
+        {
 
-            if (minimumAdditionalCosts[x] < minimum_Cost) {
-                minimum_Cost = minimumAdditionalCosts[x];
-                min_position = positions[x];
-                min_Unvisited_node = nearestNodes[x];
+            if (minimumAdditionalCosts[x] < minimumAdditionalCost)
+            {
+                minimumAdditionalCost = minimumAdditionalCosts[x];
+                minN = positions[x];
+                minUnvisited = nearestNodes[x];
             }
         }
 
-        int indexBefore = min_position == 0 ? visitedCount - 1 : min_position - 1;
-        int indexAfter = min_position + 1;
+        int indexPrevious = minN == 0 ? visitedCount - 1 : minN - 1;
+        int indexNext = minN + 1;
 
-        double distanceAfter =
-                distances[tour[min_position]][min_Unvisited_node] + distances[tour[indexAfter]][min_Unvisited_node] -
-                distances[tour[min_position]][tour[indexAfter]];
-        double distanceBefore =
-                distances[tour[min_position]][min_Unvisited_node] + distances[tour[indexBefore]][min_Unvisited_node] -
-                distances[tour[min_position]][tour[indexBefore]];
+        double distanceBefore = distanceMatrix[tour[minN]][minUnvisited] + distanceMatrix[tour[indexPrevious]][minUnvisited] -
+                distanceMatrix[tour[minN]][tour[indexPrevious]];
 
-        if (distanceAfter < distanceBefore) {
-            min_position = indexAfter;
-        } else {
-            min_position = indexBefore + 1;
+        double distanceAfter = distanceMatrix[tour[minN]][minUnvisited] + distanceMatrix[tour[indexNext]][minUnvisited] -
+                distanceMatrix[tour[minN]][tour[indexNext]];
+
+        if (distanceBefore > distanceAfter)
+        {
+            minN = indexNext;
+        }
+        else
+        {
+            minN = indexPrevious + 1;
         }
 
-        visitedCount++;
-        visited[min_Unvisited_node] = true;
-
-        for (i = visitedCount; i > min_position; i--) {
+        for (i = visitedCount; i > minN; i--)
+        {
             tour[i] = tour[i - 1];
         }
-        tour[min_position] = min_Unvisited_node;
 
-
+        tour[minN] = minUnvisited;
+        visited[minUnvisited] = true;
+        visitedCount++;
     }
 
-    for (int i = 0; i <= numOfCoords; i++) {
-        printf("%d ", tour[i]);
-        result.tour[i] = tour[i];
+    struct TourData tourData;
+    tourData.tour = malloc((numOfCoords + 1) * sizeof(int));
+
+    int count =0;
+    for(count =0; count< numOfCoords+1; count++)
+    {
+        tourData.tour[count] = tour[count];
     }
 
     double totalLength = 0;
     int i;
-    for ( i = 0; i <=numOfCoords; i++) {
-        printf("%d ", tour[i]);
-        if(i>0) {
-            totalLength += distances[tour[i]][tour[i - 1]];
+    for (i = 0; i <=numOfCoords; i++)
+    {
+        if(i>0)
+        {
+            totalLength += distanceMatrix[tour[i]][tour[i - 1]];
         }
     }
 
-    result.tourSize = totalLength;
+    tourData.tourSize = totalLength;
 
-//    double cost = 0.0;
-//    for (int i = 0; i < numOfCoords - 1; i++) {
-//        int fromVertex = tour[i];
-//        int toVertex = tour[i + 1];
-//
-//        cost += distances[fromVertex][toVertex];
-//
-//    }
-//
-//    cost += distances[tour[numOfCoords - 1]][tour[0]];
-
-//    result.tourSize = totalLength;
-
-    return result;
+    return tourData;
 }
 
